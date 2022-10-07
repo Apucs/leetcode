09755fd0419e7516e30a80540a03aafd418a316a
Apu Chandraw Shill <apu3660@gmail.com>
2022-10-07 21:09:09 +0600

    Time: 58 ms (40.17%), Space: 14 MB (26.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -3,7 +3,6 @@ class Solution:
         
         stack = []
         
-        
         for c in s:
             if c=="(" or c=="{" or c=="[":
                 stack.append(c)
@@ -20,7 +19,6 @@ class Solution:
                     stack.pop()
                     
                 else: return False
-
 
             elif c=="]":
                 if len(stack)!=0 and stack[-1]== "[":
