e72b002ca17ddc5e1e814d704466b333ebbea3c7
Apu Chandraw Shill <apu3660@gmail.com>
2022-10-09 23:40:10 +0600

    Time: 52 ms (54.84%), Space: 13.8 MB (68.01%) - LeetHub

--- leetcode/00050_powx-n/50-powx-n.py
+++ leetcode/00050_powx-n/50-powx-n.py
@@ -1,4 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
 
-        return pow(x, n)
+        def pow_cus(x, n):
+
+            # print(x, n)
+
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+
+            res = pow_cus(x*x, n//2)
+
+            val = x*res if n % 2 else res
+#             print("result for x:", res, val, x, n)
+
+            return x*res if n % 2 else res
+
+        res = pow_cus(x, abs(n))
+
+        return res if n >= 0 else 1/res
